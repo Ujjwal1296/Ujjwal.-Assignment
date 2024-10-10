0917783107b912921f76cc3f275e37d62f4e7e85
Sital silwal <ujjwalkhatri1296@gmail.com>
2024-10-10 12:38:26 +0200

    assignment

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# Ujjwal.-Assignment
+# Web_API_Rep
+All contents of Web API development
+-one 
+-two
--- script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("changeTextButton").addEventListener("click", function() {
-    document.getElementById("articleText").textContent = "The text has been changed!";
-  });
-  
